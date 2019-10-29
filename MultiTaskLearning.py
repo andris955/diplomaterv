d5bc29711f514a445073b2e62c5d383ade66415f
Tokaji Andras <955andris@gmail.com>
2019-10-29 19:09:09 +0100

    Refaktorálás

--- MultiTaskLearning.py
+++ MultiTaskLearning.py
@@ -34,10 +34,8 @@ class MultiTaskLearning:
         self.uniform_policy_steps = global_config.uniform_policy_steps  # Number of tasks to consider in the computation of r2.
         self.n = global_config.number_of_episodes_for_estimating  # Number of episodes which are used for estimating current average performance in any task Ti
 
-
         self.amta = MultiTaskAgent(algorithm, policy, self.tasks, global_config.n_steps, self.max_train_steps,
                                    n_cpus, transfer_id, tensorboard_logging=tensorboard_logging)
-
         meta_decider = None
 
         if self.algorithm == "A5C":
@@ -50,6 +48,7 @@ class MultiTaskLearning:
         self.a = []  # Average scores for every task
         self.m = []
         self.p = []  # Probability of training on an episode of task Ti next.
+        self.performance = np.zeros(len(self.tasks))
         self.tau = global_config.tau  # Temperature hyper-parameter of the softmax task-selection non-parametric policy
 
         for _ in range(len(self.tasks)):
@@ -67,13 +66,12 @@ class MultiTaskLearning:
                 if self.amta.model.train_step > self.uniform_policy_steps:
                     for j in range(len(self.tasks)):
                         self.a[j] = sum(self.s[j])/self.n
-                        self.m[j] = (self.ta[self.tasks[j]] - self.a[j]) / (self.ta[self.tasks[j]]) #* self.tau) # minél kisebb annál jobban teljesít az ágens az adott gamen
+                        self.m[j] = (self.ta[self.tasks[j]] - self.a[j]) / (self.ta[self.tasks[j]] * self.tau) # minél kisebb annál jobban teljesít az ágens az adott gamen
+                        self.performance[j] = np.min((self.a[j]) / (self.ta[self.tasks[j]]), 1)
                     for j in range(len(self.tasks)):
                         self.p[j] = np.exp(self.m[j]) / (sum(np.exp(self.m)))
                 if episode_learn % global_config.logging_frequency == 0:
-                    performance = [1-m for m in self.m]
-                    performance = [np.min([m, 1]) for m in performance]
-                    performance = np.mean(performance)  # qam
+                    performance = np.mean(self.performance)  # qam
                     self.amta.save_model(performance)
                     self.amta.flush_tbw()
                 j = np.random.choice(np.arange(0, len(self.p)), p=self.p)
@@ -85,7 +83,7 @@ class MultiTaskLearning:
             self.amta.save_model(performance)
             self.amta.exit_tbw()
 
-    def __EA4C_init(self, MetaLearningAgent, lambda_):
+    def __EA4C_init(self, meta_learning_agent, lambda_):
         self.s = []  # List of last n scores that the multi-tasking agent scored during training on task Ti.
         self.p = []  # Probability of training on an episode of task Ti next.
         self.c = np.zeros(len(self.tasks))  # Count of the number of training episodes of task Ti.
@@ -93,8 +91,8 @@ class MultiTaskLearning:
         self.r2 = 0  # First & second component of the reward for meta-learner, defined in lines 93-94
         self.reward = 0  # Reward for meta-learner
         self.s_avg = []  # Average scores for every task
-        assert isinstance(MetaLearningAgent, MetaAgent)
-        self.ma = MetaLearningAgent # Meta Learning MultiTaskAgent.
+        assert isinstance(meta_learning_agent, MetaAgent)
+        self.ma = meta_learning_agent # Meta Learning MultiTaskAgent.
         self.lambda_ = lambda_ # Lambda weighting.
         for _ in range(len(self.tasks)):
             self.p.append(1 / len(self.tasks))
@@ -103,21 +101,23 @@ class MultiTaskLearning:
         for i in range(len(self.tasks)):
             self.s_avg.append(0.0)
 
+    # TODO megcsinálni
     def __EA4C_train(self):
         performance = 0
         for train_step in range(self.max_train_steps):
-            j = self.p.index(max(self.p))
+            j = np.random.choice(np.arange(0, len(self.p)), p=self.p)
             self.c[j] = self.c[j] + 1
-            scores = self.amta.train_for_one_episode(self.tasks[j], logging=self.logging)
-            self.s[j].append(np.mean(scores))
+            ep_scores, train_steps = self.amta.train_for_one_episode(self.tasks[j], logging=self.logging)
+            self.s[j].append(np.mean(ep_scores))
             if len(self.s[j]) > self.n:
                 self.s[j].pop(0)
+            #TODO eddig jó
             for i in range(len(self.s_avg)):
                 self.s_avg[i] = sum(self.s[i])/len(self.s[i])
             s_avg_norm = [self.s_avg[i] / self.ta[self.tasks[i]] for i in range(len(self.s_avg))]
             s_avg_norm.sort()
             s_min_l = s_avg_norm[0:self.uniform_policy_steps]
-            performance = None #TODO
+            performance = None
             self.r1 = 1 - self.s_avg[j] / self.c[j]
             self.r2 = 1 - np.average(np.clip(s_min_l, 0, 1))
             self.reward = self.lambda_ * self.r1 + (1 - self.lambda_) * self.r2
